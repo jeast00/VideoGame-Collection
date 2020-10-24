73948d53eb9256aed85905afb551d2b3e729bd77
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-24 12:51:59 -0400

    added CRUD with logic in genres controller; added an index view to test associations with genres and games; added nested routes

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,55 @@
 class GenresController < ApplicationController
+
+    def index
+        if params[:user_id]
+            @user = User.find(params[:user_id])
+            @genres = @user.genres
+        else
+            @genres = Genre.all 
+        end
+    end
+
+    def new
+        @genre = Genre.new(user_id params[:user_id])
+    end
+
+    def create
+        @genre = Genre.new(genre_params)
+
+        if @genre.save
+            redirect_to genres_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @genre = Genre.find(params[:id])
+    end
+
+    def edit
+        @genre = Genre.find(params[:id])
+    end
+
+    def update
+        @genre = Genre.find(params[:id])
+
+        if @genre.update(genre_params)
+            redirect_to genres_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @genre = Genre.find(params[:id])
+        @genre.destroy
+        redirect_to genres_path
+    end
+
+    private
+
+    def genre_params
+        params.require(:genre).permit(:genre_type, :user_id, :game_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  
+  
+  
   resources :genres
-  resources :games
-  resources :users
+
+
+  resources :games do
+    resource :genres
+  end
+
+
+  resources :users do
+    resources :genres
+  end
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
