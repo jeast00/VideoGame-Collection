f1f61f93d4dbe39898ec5174297ee2d5d78cb8db
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-24 13:05:10 -0400

    Added methods and logic to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,51 @@
 class GamesController < ApplicationController
+
+    def index
+        @games = Game.all
+    end
+
+    def new
+        @game = Game.new
+    end
+
+    def show
+        @game = Game.find(params[:id])
+    end
+
+    def create
+        @game = Game.new(game_params)
+
+        if @game.save
+            redirect_to games_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @game = Game.find(params[:id])
+    end
+
+    def update
+        @game = Game.find(params[:id])
+
+        if @game.update(game_params)
+            redirect_to games_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @game = Game.find(params[:id])
+        @game.destroy
+        redirect_to games_path
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:title, :platform)
+    end
+
 end
