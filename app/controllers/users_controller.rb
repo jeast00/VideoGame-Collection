415325370153949d9750a7e5f6a4f2a80ec10e3a
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-25 14:02:00 -0400

    added methods to user controller
    added new and create methods with private strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        if user = User.create(user_params)
+            redirect_to subscriptions_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :uid, :provider)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    validates :username, :email, :password, presence: true
+    validates :username, :email, uniqueness: true
+
     has_many :genres
     has_many :games, through: :genres
     has_secure_password
