c1fe3e3e9cf9a663ff83da2b94d8b905b478c263
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-25 14:26:19 -0400

    Updated authenticate method for creating user and authenticating password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationControlller
 
     def create
         @user = User.find_by(username: params(:username))
-        if @user && @user.authenticate(password: params[:password])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
