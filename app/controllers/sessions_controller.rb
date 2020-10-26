07af7f77013441b84edeadef72cc8d7822a1ad42
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-26 00:11:06 -0400

    updated sessions controller; updated users_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,39 @@
 class SessionsController < ApplicationController
 
-    def new
-        @user = User.new
-    end
+    # def new
+    #     @user = User.new
+    # end
 
-    def create
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+    def omniauth
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)
+        end
+        if user.valid?
+            redirect_to user_path
         else
-            render :new
+            redirect_to login_path
         end
     end
 
-    def destroy
-        session.clear
-        redirect_to login_path
-    end
+    # def create
+    #     @user = User.find_by(username: params[:username])
+    #     if @user && @user.authenticate(params[:password])
+    #         session[:user_id] = @user.id
+    #         redirect_to user_path(@user)
+    #     else
+    #         render :new
+    #     end
+    # end
 
+    # def destroy
+    #     session.clear
+    #     redirect_to login_path
+    # end
+
+    private 
+    
+    def auth
+        request.env['omniauth.auth']
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,27 @@ class UsersController < ApplicationController
 
     skip_before_action :verify_authenticity_token
 
-    def new
-        @user = User.new
-    end
+    # def new
+    #     @user = User.new
+    # end
 
-    def create
-        if @user = User.create(user_params)
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            render :new
-        end
-    end
+    # def create
+    #     if @user = User.create(user_params)
+    #         session[:user_id] = @user.id
+    #         redirect_to user_path(@user)
+    #     else
+    #         render :new
+    #     end
+    # end
 
-    def show
-        @user = User.find(params[:id])
-    end
+    # def show
+    #     @user = User.find(params[:id])
+    # end
 
-    private 
+    # private 
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password, :uid, :provider)
-    end
+    # def user_params
+    #     params.require(:user).permit(:username, :email, :password, :uid, :provider)
+    # end
 
 end
